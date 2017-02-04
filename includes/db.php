5e4b7f5d57e842a0241353060745dad36777bb38
alsun <aminabdo8@gmail.com>
2017-02-05 01:20:13 +0200

    add category to admin panel

---
+++ includes/db.php
@@ -0,0 +1,25 @@
+<?php
+
+$db['db_host'] = 'localhost';
+$db['db_admin'] = 'root';
+$db['db_pass'] = '';
+$db['db_name'] = 'cms';
+
+foreach($db as $key => $value){
+
+    define(strtoupper($key),$value);
+
+}
+
+
+$connection = mysqli_connect(DB_HOST,DB_ADMIN,DB_PASS,DB_NAME);
+
+
+
+
+
+
+
+
+
+?>
